52854afba6f1b05e2046dce27590b96734af7e4f
MaddyWithaY-coder <madelinejaneray@gmail.com>
2026-07-07 18:17:48 -0400

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reference.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//When I type a word the word doesn't show options helppppp
+//Guys it got fixed lsg
+
+@TeleOp
+public class Reference extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 21865;
+        double motorSpeed = 0.5;
+        boolean touchSensorPressed = true;
+
+        telemetry.addData("Team Number", teamNumber);
+            telemetry.addData("Motor Speed",motorSpeed);
+            telemetry.addData("Touch Sensor", touchSensorPressed);
+    }
+  @Override
+    public void loop()   {
+        telemetry.addData("Left stick x", gamepad1.left_stick_x);
+        telemetry.addData("Right stick y", gamepad1.left_stick_y);
+        telemetry.addData("A button", gamepad1.a);
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        telemetry.addData("speedForward", speedForward);
+
+
+
+  }
+}
