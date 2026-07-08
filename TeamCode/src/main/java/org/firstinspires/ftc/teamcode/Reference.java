6f3ad8c258da14bace9a8b25740e26db2d090648
MaddyWithaY-coder <madelinejaneray@gmail.com>
2026-07-07 20:12:25 -0400

    Second Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reference.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Reference.java
@@ -12,18 +12,20 @@ public class Reference extends OpMode {
         double motorSpeed = 0.5;
         boolean touchSensorPressed = true;
 
-        telemetry.addData("Team Number", teamNumber);
+          telemetry.addData("Team Number", teamNumber);
             telemetry.addData("Motor Speed",motorSpeed);
             telemetry.addData("Touch Sensor", touchSensorPressed);
     }
   @Override
     public void loop()   {
         telemetry.addData("Left stick x", gamepad1.left_stick_x);
-        telemetry.addData("Right stick y", gamepad1.left_stick_y);
+        telemetry.addData("Right stick y", gamepad1.right_stick_y);
         telemetry.addData("A button", gamepad1.a);
         double speedForward = -gamepad1.left_stick_y / 2.0;
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
         telemetry.addData("speedForward", speedForward);
+
 
 
 
